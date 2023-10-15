3781fbd06fe606f3fe11c4a87c80af923869c76b
raquel15rs <102822903+raquel15rs@users.noreply.github.com>
2023-10-14 21:34:16 -0300

    Funções Numéricas

--- 4_bimestre/funcoes.sql
+++ 4_bimestre/funcoes.sql
@@ -1,3 +1,4 @@
+--Funções de String--
 CREATE TABLE nomes(
 	nome VARCHAR(255)
 );
@@ -12,3 +13,18 @@ SELECT UPPER(nome) AS nomes FROM nomes;
 SELECT nome, LENGTH(nome) AS tamanho FROM nomes;
 
 SELECT CONCAT(IF(nome = 'Maria Clara' or nome = 'Roberta', 'Sra. ', 'Sr. '), nome) AS nome FROM nomes;
+--Funções de String--
+
+--Funções Numéricas--
+CREATE TABLE produtos(
+	produto VARCHAR(255),
+	preco DECIMAL(15,3),
+	quantidade INT
+);
+
+SELECT produto, ROUND(preco, 2) AS preco_arredondado FROM produtos;
+
+SELECT produto, ABS(quantidade) AS quantidade_absoluta FROM produtos;
+
+SELECT produto, AVG(preco) AS preco_medio FROM produtos GROUP BY produto;
+--Funções Numéricas--
