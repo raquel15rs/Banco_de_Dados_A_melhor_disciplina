881efe86432fa6bfeca7ad2c9555d3e36ba444c6
raquel15rs <102822903+raquel15rs@users.noreply.github.com>
2023-10-25 23:18:15 -0300

    2

--- 4_bimestre/exerciciosDeFixacao20231025.sql
+++ 4_bimestre/exerciciosDeFixacao20231025.sql
@@ -2,3 +2,8 @@ CREATE TRIGGER apos_inserir_clientes
   BEFORE INSERT ON Clientes
 	FOR EACH ROW 
 INSERT INTO Auditoria (mensagem) VALUES ('Concluído');
+
+CREATE TRIGGER antes_deletar_clientes
+	AFTER DELETE ON Clientes
+	FOR EACH ROW
+INSERT INTO Auditoria (mensagem) VALUES ('Excluído');
