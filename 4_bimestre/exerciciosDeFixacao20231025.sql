35721d4f89230b10e0cc57f574b0a712ed08e16b
raquel15rs <102822903+raquel15rs@users.noreply.github.com>
2023-10-25 23:21:02 -0300

    4

--- 4_bimestre/exerciciosDeFixacao20231025.sql
+++ 4_bimestre/exerciciosDeFixacao20231025.sql
@@ -12,3 +12,14 @@ CREATE TRIGGER depois_atualizar_clientes
 	BEFORE UPDATE ON Clientes
 	FOR EACH ROW
 INSERT INTO Auditoria (mensagem) VALUES (concat(old.nome, new.nome));
+
+DELIMITER //
+CREATE TRIGGER cliente_vazio
+	BEFORE UPDATE ON Clientes
+	FOR EACH ROW
+	IF new.nome is NULL or new.nome = '' THEN
+		UPDATE Clientes SET nome = old.nome;
+		INSERT INTO Auditoria (mensagem) VALUES ('Cliente vazio');
+	END IF;
+//
+DELIMITER ;
